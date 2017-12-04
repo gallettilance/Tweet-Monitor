e5ffb58f65e31dae24f72b4e89b1a0c6258d0587
galletti94 <galletti@bu.edu>
2017-12-03 22:49:05 -0500

    Updating: adding location printing

--- access_mongodb_database.py
+++ access_mongodb_database.py
@@ -9,6 +9,7 @@ for tweet in tweets_iterator:
   print('tweet text: ',tweet['text'])
   print('user\'s screen name: ',tweet['user']['screen_name'])
   print('user\'s name: ',tweet['user']['name'])
+  print('location: ', tweet['user']['location'])
   try:
     print('retweet count: ',tweet['retweeted_status']['retweet_count'])
     print('retweeter\'s name: ', tweet['retweeted_status']['user']['name'])
