307545f25551538496d9f50dc1339fa8c44c71f7
galletti94 <galletti@bu.edu>
2017-12-05 16:49:23 -0500

    Updating: minor update on emoji extraction

--- access_mongodb_database.py
+++ access_mongodb_database.py
@@ -4,16 +4,18 @@ from textblob.sentiments import NaiveBayesAnalyzer
 from emoji import UNICODE_EMOJI
 
 
-
 client = MongoClient('localhost', 27017)
 db = client['usa_db']
 collection = db['usa_tweets_collection']
 tweets_iterator = collection.find()
 
+
+#count emoji occurences
+'''
 d = dict()
 i = 0
 for tweet in tweets_iterator:
-  for word in tweet['text'].split():
+  for word in list(tweet['text']):
     if word in UNICODE_EMOJI:
       try:
         d[word] += 1
@@ -22,8 +24,69 @@ for tweet in tweets_iterator:
 
 d = sorted(d.items(), key=lambda x: -x[1])
 print(d[:15])
+'''
 
-      
+#count specific emoji usage per state
+'''
+d = dict()
+i = 0
+for tweet in tweets_iterator:
+  for word in list(tweet['text']):
+    if word in UNICODE_EMOJI:
+      if word == '🎄' and tweet['user']['location'] is not None:
+        if ',' in list(tweet['user']['location']):
+          state = tweet['user']['location'].split(',')[1]
+          try:
+            d[state] += 1
+          except KeyError:
+            d[state] = 1
+        
+d = sorted(d.items(), key=lambda x: -x[1])
+print(d[:5])
+'''
+
+#count emoji usage for MA
+'''
+d = dict()
+i = 0
+for tweet in tweets_iterator:
+  for word in list(tweet['text']):
+    if word in UNICODE_EMOJI and tweet['user']['location'] is not None:
+      if ',' in list(tweet['user']['location']):
+          state = tweet['user']['location'].split(',')[1]
+          if state == 'MA' or state == 'Massachussetts':
+            try:
+              d[word] += 1
+            except KeyError:
+              d[word] = 1
+
+d = sorted(d.items(), key=lambda x: -x[1])
+print(d[:5])
+'''
+
+
+
+#count emoji usage per state
+'''
+d = dict()
+i = 0
+for tweet in tweets_iterator:
+  for word in list(tweet['text']):
+    if word in UNICODE_EMOJI:
+      if tweet['user']['location'] is not None:
+        if ',' in list(tweet['user']['location']):
+          state = tweet['user']['location'].split(',')[1]
+          try:
+            d[state] += 1
+          except KeyError:
+            d[state] = 1
+        
+d = sorted(d.items(), key=lambda x: -x[1])
+print(d[:5])
+'''
+
+
+#sentiment analysis      
 '''
 client = MongoClient('localhost', 27017)
 db = client['twitterdb']
@@ -46,7 +109,7 @@ for tweet in tweets_iterator:
     if blob.sentiment.classification == 'neu':
       print('neutral sentiment for the tweet: ', tweet['text'])
 
-  
+  #print some of the tweet content
   print('tweet text: ',tweet['text'])
   print('user\'s screen name: ',tweet['user']['screen_name'])
   print('user\'s name: ',tweet['user']['name'])
